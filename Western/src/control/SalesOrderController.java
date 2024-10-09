2cc9dc750f7f3df5ff731401fc4a2bdbb8da73a3
ArakinJojo <davidkalok0911@gmail.com>
2024-10-09 21:05:05 +0200

    Update SalesOrderController.java

--- Western/src/control/SalesOrderController.java
+++ Western/src/control/SalesOrderController.java
@@ -1,6 +1,7 @@
 package control;
 
 import model.Customer;
+
 import model.Product;
 import model.SalesOrder;
 
@@ -8,19 +9,30 @@ public class SalesOrderController {
 
 	private SalesOrder salesOrder;
 	
+	public SalesOrderController() {
+		createSaleOrder();
+	}
+	
 	public SalesOrder createSaleOrder() {
-		
-		return null;
+		if(salesOrder == null) {
+			this.salesOrder = new SalesOrder();
+		}
+		return salesOrder;
 	}
 	
 	public Customer addCustomerToOrder(String customerNo) {
+		CustomerController customerController = new CustomerController();
+		Customer customer = customerController.findCustomerByCustomerNumber(customerNo);
 		
-		return null;
+		return customer;
+		
 	}
 	
 	public Product addProductToOrder(String barcode) {
+		ProductController productController = new ProductController();
+		Product product = productController.findProductByBarcode(barcode, false);
 		
-		return null;
+		return product;
 	}
 	
 	public SalesOrder finishSaleOrder() {
