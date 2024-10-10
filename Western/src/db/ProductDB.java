761f7364b3189ff06ed0b43d1b9825f05f2b572a
UCNafj92 <10503113@ucn.dk>
2024-10-10 10:02:45 +0200

    productdb comm diagram

--- Western/src/db/ProductDB.java
+++ Western/src/db/ProductDB.java
@@ -2,12 +2,8 @@ package db;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
 
-import model.Order;
 import model.Product;
 
 public class ProductDB implements ProductDBIF {
@@ -15,21 +11,22 @@ public class ProductDB implements ProductDBIF {
 	private static final String checkUpdateQ = "";
 	private static final String findProductTypeByProductIdQ = "SELECT Product_id, 'CLOTHING' AS tableName from CLOTHING where Product_id = ? "
 			+ "UNION SELECT Product_id, 'EQUIPMENT' AS tableName FROM EQUIPMENT WHERE Product_id = ? UNION SELECT Product_id, 'GUN_REPLICA' AS tableName from GUN_REPLICA WHERE Product_id = ?";
-	
+
 	private PreparedStatement findProductByBarcodeStatement;
 	private PreparedStatement checkUpdate;
 	private PreparedStatement findProductTypeByProductIdStatement;
 	private DBConnection dbConnection;
-	
+
 	public ProductDB() throws SQLException, DataAccessException {
 		dbConnection = DBConnection.getInstance();
-		findProductByBarcodeStatement = dbConnection.getConnection().prepareStatement(findProductByBarcodeQ, Statement.RETURN_GENERATED_KEYS);
-		findProductTypeByProductIdStatement = dbConnection.getConnection().prepareStatement(findProductTypeByProductIdQ, Statement.RETURN_GENERATED_KEYS);
+		findProductByBarcodeStatement = dbConnection.getConnection().prepareStatement(findProductByBarcodeQ);
+		findProductTypeByProductIdStatement = dbConnection.getConnection()
+				.prepareStatement(findProductTypeByProductIdQ);
 	}
 
 	@Override
 	public Product findProductByBarcode(String barcode) {
-		
+
 		Product p = null;
 		try {
 			findProductByBarcodeStatement.setString(1, barcode);
@@ -44,26 +41,22 @@ public class ProductDB implements ProductDBIF {
 
 		return p;
 	}
-	
 
+	private Product buildObject(ResultSet rs, boolean retrieveAssociation) throws DataAccessException {
+		Product product = null;
 
-private Product buildObject(ResultSet rs, boolean retrieveAssociation) throws DataAccessException {
-	Product product = null;
-	
-	try {
-		int productId = rs.getInt(1);
-		ResultSet rs = findProductTypeByProductIdStatement.executeQuery();		
+		try {
+			int productId = rs.getInt(1);
+			ResultSet rs = findProductTypeByProductIdStatement.executeQuery();
 
-		
-		
-		product = new Product   (Id,   rs.getString(2), rs.getDate(3).toLocalDate(), rs.getString(4));
-		if (retrieveAssociation) {
-			product.setOrderlines(orderLineDB.findAllOrderLine(orderId));
+			product = new Product(Id, rs.getString(2), rs.getDate(3).toLocalDate(), rs.getString(4));
+			if (retrieveAssociation) {
+				product.setOrderlines(orderLineDB.findAllOrderLine(orderId));
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException("Cannot convert from ResultSet", e);
 		}
-	} catch (SQLException e) {
-		throw new DataAccessException("Cannot convert from ResultSet", e);
+		return product;
 	}
-	return product;
-}
 
 }
